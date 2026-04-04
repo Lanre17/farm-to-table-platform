f79f1eaf6e0700df81c7ae3b9b2135c8d524556a
henrylapham <lapham.h@northeastern.edu>
2026-04-03 22:45:12 -0400

    added comment notes to explain code in Ecosystem.java, Enterprise.java, Network.java, WorkRequest.java, WorkRequestDirectory.java

--- FarmToTableNetwork/src/main/java/com/mycompany/farmtotablenetwork/ecosystem/Network.java
+++ FarmToTableNetwork/src/main/java/com/mycompany/farmtotablenetwork/ecosystem/Network.java
@@ -10,35 +10,45 @@ import java.util.ArrayList;
  *
  * @author Hank_Local
  */
+
+//HL: Represents Regional Distribution Network (2nd level of hierarchy) 
+//HL: container for all 4 enterprise objects (Farm/Producer, Quality & Inspection Agency, Distribution/Logistics, Retail Store/Restaurant)
+
 public class Network {
-     private static int count = 0;
-
+    private static int count = 0;
     private int networkId;
-
     private String name;
 
+    //HL: list of enterprise objects
     private ArrayList<Enterprise> enterprises = new ArrayList<>();
 
-    public Network(String name) { this.networkId = ++count; this.name = name; }
+    public Network(String name) { 
+        this.networkId = ++count; this.name = name; 
+    }
 
-    public void addEnterprise(Enterprise e) { enterprises.add(e); }
+    public void addEnterprise(Enterprise e) { 
+        enterprises.add(e); 
+    }
 
-    public ArrayList<Enterprise> getEnterprises() { return enterprises; }
+    public ArrayList<Enterprise> getEnterprises() { 
+        return enterprises; 
+    }
 
-    public int    getNetworkId() { return networkId; }
+    //HL: getters
+    public int getNetworkId() { 
+        return networkId; 
+    }
 
-    public String getName()      { return name; }
+    public String getName() { 
+        return name; 
+    }
 
+    //HL: method that finds enterprise by ID 
     public Enterprise findEnterprise(int id) {
-
         for (Enterprise e : enterprises) {
-
             if (e.getEnterpriseId() == id) return e;
-
         }
-
-        return null;
-
+        return null; //HL: null check
     }
 
 }

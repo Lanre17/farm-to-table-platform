e3980d0fcc593469c2fd0e98c96c1bac59ed0d2c
Polina Starobinets <p.starobinets@northeastern.edu>
2026-04-05 18:47:17 -0400

    Harvest WorkerWorkArea, CreateBatchPanel + wiring to ConfigureABusiness and MainFrame

--- FarmToTableNetwork/src/main/java/com/mycompany/farmtotablenetwork/ConfigureABusiness.java
+++ FarmToTableNetwork/src/main/java/com/mycompany/farmtotablenetwork/ConfigureABusiness.java
@@ -35,20 +35,28 @@ public class ConfigureABusiness {
     public static WorkRequestDirectory workRequestDirectory = new WorkRequestDirectory();       //ps added 4/4/26
     
     //Enterprise Directories
+    //Farm & Harvest
     public static CropDirectory cropDirectory = new CropDirectory();        //ps added 4/4/26
     public static HarvestBatchDirectory batchDirectory = new HarvestBatchDirectory();       // ps added 4/4/26
+    
+    //Inspection & Certification
+    public static CertificationDirectory certDirectory = new CertificationDirectory(); // EC added
+    public static InspectionDirectory inspectionDirectory = new InspectionDirectory(); // EC added
+    
+    //Distribution & Logistics
     public static ShipmentReceiptConfirmationDirectory receiptDirectory = new ShipmentReceiptConfirmationDirectory(); //HL: added import using AltEnter, 4/4/26
     
+    //Retail& Procurement
     // Shared in-memory storage for all retail purchase orders
     public static PurchaseOrderDirectory orderDirectory = new PurchaseOrderDirectory();  //LY added 4/4/26
-
     // Shared in-memory storage for all stocked inventory items
     public static InventoryDirectory inventoryDirectory = new InventoryDirectory(); //LY added 4/4/26
     
+        
     //Organization references
     public static Organization cropMgmt;                            //ps added 4/4/26
     public static Organization harvestAndPackaging;                 //ps added 4/4/26
-    public static Organization inspectionDept;                       //ps added 4/4/26
+    public static Organization inspectionDept;                      //ps added 4/4/26
     public static Organization certificationDept;                   //ps added 4/4/26
     public static Organization warehouseOps;                        //ps added 4/4/26
     public static Organization fleetMgmt;                           //ps added 4/4/26
@@ -56,26 +64,22 @@ public class ConfigureABusiness {
     public static Organization storefrontInventory;                 //ps added 4/4/26
    
     
-    
-    public static CertificationDirectory certDirectory = new CertificationDirectory(); // EC added
-    public static InspectionDirectory inspectionDirectory = new InspectionDirectory(); // EC added
-       
-    
+   
     public static void configure() {
-        Enterprise farmEnterprise = new Enterprise("Farm/Producer");                            //ps added 4/4/26
-        Enterprise inspectionEnterprise   = new Enterprise("Quality & Inspection Agency");      //ps added 4/4/26
-        Enterprise distributionEnterprise = new Enterprise("Distribution / Logistics Co.");     //ps added 4/4/26
-        Enterprise retailEnterprise       = new Enterprise("Retail Store / Restaurant");        //ps added 4/4/26
+        Enterprise farmEnterprise           = new Enterprise("Farm/Producer");                    //ps added 4/4/26
+        Enterprise inspectionEnterprise     = new Enterprise("Quality & Inspection Agency");      //ps added 4/4/26
+        Enterprise distributionEnterprise   = new Enterprise("Distribution / Logistics Co.");     //ps added 4/4/26
+        Enterprise retailEnterprise         = new Enterprise("Retail Store / Restaurant");        //ps added 4/4/26
 
         
-        cropMgmt = new Organization ("Crop Management", farmEnterprise.getEnterpriseId());                              //ps added 4/4/26
-        harvestAndPackaging = new Organization ("Harvest & packaging", farmEnterprise.getEnterpriseId());               //ps added 4/4/26
-        inspectionDept = new Organization("Inspection Department", inspectionEnterprise.getEnterpriseId());             //ps added 4/4/26
-        certificationDept = new Organization("Certification Department", inspectionEnterprise.getEnterpriseId());       //ps added 4/4/26
-        warehouseOps         = new Organization("Warehouse Operations", distributionEnterprise.getEnterpriseId());      //ps added 4/4/26
-        fleetMgmt            = new Organization("Fleet / Delivery Management", distributionEnterprise.getEnterpriseId());//ps added 4/4/26
-        procurement          = new Organization("Procurement / Purchasing", retailEnterprise.getEnterpriseId());        //ps added 4/4/26
-        storefrontInventory  = new Organization("Storefront / Inventory", retailEnterprise.getEnterpriseId());          //ps added 4/4/26
+        cropMgmt            = new Organization ("Crop Management", farmEnterprise.getEnterpriseId());                       //ps added 4/4/26
+        harvestAndPackaging = new Organization ("Harvest & packaging", farmEnterprise.getEnterpriseId());                   //ps added 4/4/26
+        inspectionDept      = new Organization("Inspection Department", inspectionEnterprise.getEnterpriseId());            //ps added 4/4/26
+        certificationDept   = new Organization("Certification Department", inspectionEnterprise.getEnterpriseId());         //ps added 4/4/26
+        warehouseOps        = new Organization("Warehouse Operations", distributionEnterprise.getEnterpriseId());           //ps added 4/4/26
+        fleetMgmt           = new Organization("Fleet / Delivery Management", distributionEnterprise.getEnterpriseId());    //ps added 4/4/26
+        procurement         = new Organization("Procurement / Purchasing", retailEnterprise.getEnterpriseId());             //ps added 4/4/26
+        storefrontInventory = new Organization("Storefront / Inventory", retailEnterprise.getEnterpriseId());               //ps added 4/4/26
 
         
         

29f6e23c81ac9a28ffe143bbe699a6fa8d668996
emmanuotne <ouldtayeb.e@northeastern.edu>
2026-04-07 14:48:11 -0700

    add RecordResultPanel and CertifierWorkArea stubs

---
+++ FarmToTableNetwork/src/main/java/com/mycompany/farmtotablenetwork/ui/inspection/CertifierWorkArea.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.farmtotablenetwork.ui.inspection;
+
+import com.mycompany.farmtotablenetwork.personnel.profiles.CertifierProfile;
+import com.mycompany.farmtotablenetwork.ui.main.CardSequencePanel;
+import javax.swing.JPanel;
+/**
+ *
+ * @author emmanuelcroll
+ */
+
+
+// stub — full implementation coming in 5.10
+public class CertifierWorkArea extends JPanel {
+    public CertifierWorkArea(CertifierProfile profile, CardSequencePanel cardPanel) {
+    }
+}

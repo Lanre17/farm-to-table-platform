529ae0061dce338c0ee12a3d4aa5b2bf792beb3a
henrylapham <lapham.h@northeastern.edu>
2026-04-12 10:05:07 -0400

    added enterprise admins to seedAuth() method (one per enterprise, 4 total (cropMgmt, inspectionDept, waerhouseOps, procurement)

--- FarmToTableNetwork/src/main/java/com/mycompany/farmtotablenetwork/ConfigureABusiness.java
+++ FarmToTableNetwork/src/main/java/com/mycompany/farmtotablenetwork/ConfigureABusiness.java
@@ -26,6 +26,7 @@ import com.mycompany.farmtotablenetwork.inspection.CertificationDirectory;
 import com.mycompany.farmtotablenetwork.inspection.InspectionDirectory;
 import com.mycompany.farmtotablenetwork.personnel.profiles.CertifierProfile;
 import com.mycompany.farmtotablenetwork.personnel.profiles.DeliveryDriverProfile;
+import com.mycompany.farmtotablenetwork.personnel.profiles.EnterpriseAdminProfile;
 import com.mycompany.farmtotablenetwork.personnel.profiles.InspectorProfile;
 import com.mycompany.farmtotablenetwork.personnel.profiles.InventoryClerkProfile;
 import com.mycompany.farmtotablenetwork.personnel.profiles.ProcurementOfficerProfile;
@@ -185,6 +186,22 @@ public class ConfigureABusiness {
         SystemAdminProfile saProfile = new SystemAdminProfile(saPerson, cropMgmt); //HL: added import using AltEnter 
         accountDirectory.newAccount("admin", "admin", saProfile);
         
+        //HL: Enterprise Admin Profile (one per enterprise, 4 total) 
+        Person ea1Person = new Person(faker.name().firstName(), faker.name().lastName(), faker.internet().emailAddress(), faker.phoneNumber().cellPhone());
+        EnterpriseAdminProfile ea1Profile = new EnterpriseAdminProfile(ea1Person, cropMgmt); //HL: added import using AltEnter 
+        accountDirectory.newAccount("farm_admin", "pass", ea1Profile);
+        
+        Person ea2Person = new Person(faker.name().firstName(), faker.name().lastName(), faker.internet().emailAddress(), faker.phoneNumber().cellPhone());
+        EnterpriseAdminProfile ea2Profile = new EnterpriseAdminProfile(ea2Person, inspectionDept);
+        accountDirectory.newAccount("inspection_admin", "pass", ea2Profile);
+        
+        Person ea3Person = new Person(faker.name().firstName(), faker.name().lastName(), faker.internet().emailAddress(), faker.phoneNumber().cellPhone());
+        EnterpriseAdminProfile ea3Profile = new EnterpriseAdminProfile(ea3Person, warehouseOps);
+        accountDirectory.newAccount("dist_admin", "pass", ea3Profile);
+        
+        Person ea4Person = new Person(faker.name().firstName(), faker.name().lastName(), faker.internet().emailAddress(), faker.phoneNumber().cellPhone());
+        EnterpriseAdminProfile ea4Profile = new EnterpriseAdminProfile(ea4Person, procurement);
+        accountDirectory.newAccount("retail_admin", "pass", ea4Profile);
 
     }
     
